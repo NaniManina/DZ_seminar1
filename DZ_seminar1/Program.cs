ec30cb837944b4d8718a77b384ce5abfb87425f8
NaniManina <nani.manina999@mail.ru>
2022-11-27 20:41:01 +0500

    зачада на макс из 3 чисел, не удалось синтаксически  создать условия равенства

---
+++ DZ_seminar1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.Write("ВВедите число a = ");
+// double a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("ВВедите число b = ");
+// double b = Convert.ToInt32(Console.ReadLine());
+// if (a > b) 
+// {
+//     Console.Write("Максимальное a");
+// }
+// else if (a == b)
+// {
+//      Console.Write("Максимального числа нет, a=b");
+// }
+
+// else Console.Write("Максимальное b");
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("ВВедите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("ВВедите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("ВВедите число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write(max);
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+
